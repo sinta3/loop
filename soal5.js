08f91eaafe69018811d43a5576df8a2ee8f32176
sinta3 <sinta.tri38@gmail.com>
2020-06-11 20:24:46 +0700

    soal5.js

--- soal5.js
+++ soal5.js
@@ -1,6 +1,7 @@
-let input = prompt ('masukkan kepanjangan dari IB');
+let input = prompt ('masukkan kepanjangan dari IB');;
 
 while (input !== 'Impact Byte') {
-    let input = prompt ('masukkan kepanjangan dari IB');
+    input = prompt ('masukkan kepanjangan dari IB');
+    
 }
 alert('selamat anda benar');
